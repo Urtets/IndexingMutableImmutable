5ffefd73b3b1977b6a1bd5f6e9077f7554f20ffb
Юрий <144734719+Urtets@users.noreply.github.com>
2024-08-24 20:55:32 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 56, 'Casper', 56.12
+print(immutable_var)
+# immutable_var[1] = 'Jasper' - неизменяемый тип
+mutable_list = [35, 'Casper-Dog', 10.14]
+print(mutable_list)
+mutable_list[0] = 36
+mutable_list[1] = 'Super-Dog'
+mutable_list[2] = 'He is the best'
+print(mutable_list)
